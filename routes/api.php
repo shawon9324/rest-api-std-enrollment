a00feadefabce06333de6fb84b9e18d8d86e8ea4
shawonrog <shahriar15-1269@diu.edu.bd>
2020-11-11 22:21:07 +0600

    JWT token:Authentication Login

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,18 @@ Route::ApiResource('/subject','Api\SubjectController');
 Route::ApiResource('/section','Api\SectionController');
 Route::ApiResource('/student','Api\StudentController');
 
+Route::group([
+
+    'prefix' => 'auth'
+
+], function () {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
